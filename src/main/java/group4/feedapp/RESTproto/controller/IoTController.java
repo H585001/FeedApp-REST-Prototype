84e1e306e6c3ab25416e4f3321b04ce8bfa3f112
H585001 <54100104+H585001@users.noreply.github.com>
2022-10-07 12:52:09 +0200

    Add files via upload

---
+++ src/main/java/group4/feedapp/RESTproto/controller/IoTController.java
@@ -0,0 +1,44 @@
+package group4.feedapp.RESTproto.controller;
+import java.util.Collection;
+
+import org.springframework.web.bind.annotation.*;
+
+import group4.feedapp.RESTproto.model.IoTDevice;
+import group4.feedapp.RESTproto.model.IoTVotes;
+import group4.feedapp.RESTproto.model.Poll;
+import group4.feedapp.RESTproto.service.IoTDeviceService;
+import group4.feedapp.RESTproto.service.PollService;
+
+@RestController
+public class IoTController {
+	
+	private final IoTDeviceService deviceService;
+	private final PollService pollService;
+	
+	public IoTController(IoTDeviceService deviceService, PollService pollService) {
+		this.deviceService = deviceService;
+		this.pollService = pollService;
+	}
+	
+	@PutMapping("/iot/{id}")
+	public Poll connectDeviceToPoll(@PathVariable Long id, @RequestBody Long pollId) {
+		Poll poll = pollService.getPoll(pollId);
+		return deviceService.connectDeviceToPoll(id, poll);
+	}
+	
+	@GetMapping("/iot/{id}/poll")
+	public Poll getConnectedPoll(@PathVariable Long id) {
+		return deviceService.getConnectedPoll(id);
+	}
+	
+	@GetMapping("/iot")
+	public Collection<IoTDevice> getAllDevices() {
+		return deviceService.getAllDevices();
+	}
+	
+	@PostMapping("/iot/{id}/vote")
+	public IoTVotes iotVoteOnPoll(@PathVariable Long id, @RequestBody IoTVotes votes) {
+		return pollService.voteOnPoll(deviceService.getConnectedPoll(id).getId(), id, votes);
+	}
+	
+}
---
+++ src/main/java/group4/feedapp/RESTproto/service/IoTDeviceService.java
@@ -0,0 +1,40 @@
+package group4.feedapp.RESTproto.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import group4.feedapp.RESTproto.dao.IoTDeviceDAO;
+import group4.feedapp.RESTproto.dao.IoTVotesDAO;
+import group4.feedapp.RESTproto.model.IoTDevice;
+import group4.feedapp.RESTproto.model.Poll;
+
+@Service
+public class IoTDeviceService {
+	
+	private IoTDeviceDAO iotDao;
+	
+	@Autowired
+	public IoTDeviceService(IoTDeviceDAO iotDao) {
+		this.iotDao = iotDao;
+	}
+	
+	public Poll connectDeviceToPoll(Long id, Poll poll) {
+		IoTDevice device = iotDao.readIoTDevice(id);
+		device.setLinkedPoll(poll);
+		iotDao.updateIoTDevice(id, device);
+		return poll;
+	}
+	
+	public Poll getConnectedPoll(Long id) {
+		return iotDao.readIoTDevice(id).getLinkedPoll();
+	}
+	
+	public Collection<IoTDevice> getAllDevices() {
+		return iotDao.readIoTDevices();
+	}
+	
+	
+
+}
